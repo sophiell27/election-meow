f952124d2f7110c3ae287a640f370118c6b0a419
sophiee <105938870+sophiell27@users.noreply.github.com>
2023-11-18 20:50:12 +0800

    chore: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src/')
-        }
-    }
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/election-meow/' : '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
 });
